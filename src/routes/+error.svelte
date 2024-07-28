7fd10202952138293fff0224a8062f4ce89c24b5
หนมจีบ <onopingping@gmail.com>
2024-07-28 20:58:57 +0700

    🐛 fix : 404 not found template

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,5 @@
 <main class="w-screen h-screen bg-black grid place-items-center">
 	<div>
 		<div class="text-white text-center">404 Not Found</div>
-		<div class="text-white">What do you want from single page web application?</div>
 	</div>
 </main>
