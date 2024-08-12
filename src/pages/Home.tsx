310bc28f5f786d03adae6a73775be56c77f305dc
SSSBoOm <boom1909chanapat@gmail.com>
2024-08-12 22:18:46 +0700

    fix: style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react'
 
-const Home = () => {
-  const eight = [1, 2, 3, 4, 5, 6, 7, 8];
+const Home: React.FC = () => {
+  const eight = [1, 2, 3, 4, 5, 6, 7, 8]
   return (
-    <>
-      <main className="bg-slate-200 grid place-items-center h-screen w-screen">
+    <React.Fragment>
+      <div className="bg-slate-200 grid place-items-center h-full w-full">
         <div className="grid grid-cols-2 bg-primary-blue h-5/6 w-3/4">
           {eight.map((item) => {
             return (
@@ -12,22 +12,20 @@ const Home = () => {
                 className="bg-primary-blue hover:bg-secondary-blue cursor-pointer grid place-items-center"
                 key={item}
                 onClick={() => {
-                  console.log(item);
+                  console.log(item)
                 }}
               >
                 <div className="text-center">
-                  <h1 className="text-center text-2xl font-bold ">
-                    กล่องที่ {item}
-                  </h1>
+                  <h1 className="text-center text-2xl font-bold ">กล่องที่ {item}</h1>
                   <h2>ชื่อยา</h2>
                 </div>
               </div>
-            );
+            )
           })}
         </div>
-      </main>
-    </>
-  );
-};
+      </div>
+    </React.Fragment>
+  )
+}
 
-export default Home;
+export default Home
