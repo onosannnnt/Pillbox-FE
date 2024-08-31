c6eba7bf5a9786c14c9221449508de3388963f5f
onosannnnt <onopingping@gmail.com>
2024-09-01 04:05:14 +0700

    ✨ feat : Pill detail page

--- src/config/route.ts
+++ src/config/route.ts
@@ -4,4 +4,4 @@ export const HISTORY_ROUTE = "/history";
 export const PILL_STOCK_ROUTE = "/pillstock";
 export const LOGIN_ROUTE = "/login";
 export const FORGOTTEN_RATE_ROUTE = "/forgotten-rate";
-export const PILL_DETAIL_ROUTE = "/pill-detail/:id";
+export const PILL_DETAIL_ROUTE = "/pill-detail/:pillID";
--- src/pages/PillDetail.tsx
+++ src/pages/PillDetail.tsx
@@ -1,9 +1,118 @@
-import React from "react";
+import Loading from "@/components/Loading";
+import { axiosInstance } from "@/utils/axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+
+type pillData = {
+  id: string;
+  channelIndex: number;
+  amount: number;
+  Total: number;
+  amountPerTime: number;
+  medicine: {
+    id: string;
+    name: string;
+    description: string;
+    note: string;
+    img: string;
+  };
+  times: {
+    time: string;
+  }[];
+};
 
 const PillDetail = () => {
+  const { pillID } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [pill, setPill] = useState<pillData>();
+
+  const fetchPill = async () => {
+    try {
+      const response = await axiosInstance.get(
+        `/user/getPillChannel/${pillID}`
+      );
+      setPill(response.data);
+      setIsLoading(false);
+    } catch {
+      Swal.fire({
+        icon: "error",
+        title: "เกิดข้อผิดพลาด",
+        text: "กรุณาลองใหม่อีกครั้ง",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchPill();
+    console.log(pill?.times.map((time) => time.time));
+  }, [isLoading]);
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="w-full h-screen content-center text-center">
+          <Loading size="large" />
+        </div>
+      </>
+    );
+  }
   return (
     <>
-      <div>Pill Detail</div>
+      <h1 className="text-4xl text-center p-5">
+        ข้อมูลยาช่องที่{" "}
+        {pill?.channelIndex !== undefined ? pill.channelIndex + 1 : ""}
+      </h1>
+      <div className="flex flex-col divide-y-8">
+        {" "}
+        <div className="flex flex-col items-center gap-x-2">
+          <div className="flex w-1/2 px-10 py-4">
+            <b className="text-2xl px-6">ชื่อยาสามัญ : </b>
+            <div className="text-2xl">{pill?.medicine.name}</div>
+          </div>
+          <div className="flex w-1/2 px-10 py-4">
+            <b className="font-bold text-2xl px-6 text-right">
+              จำนวนยาที่เหลือ :{" "}
+            </b>
+            <div className="text-2xl">{pill?.amount} เม็ด</div>
+          </div>
+          <div className="flex w-1/2 px-10 py-4">
+            <b className="font-bold text-2xl px-6">
+              จำนวนยาที่ต้องทานในแต่ละครั้ง :{" "}
+            </b>
+            <div className="text-2xl">{pill?.amountPerTime} เม็ด/มื้อ</div>
+          </div>
+          <div className="flex w-1/2 px-10 py-4">
+            <b className="font-bold text-2xl px-6">เวลาทานยา : </b>
+            <div className="grid gap-3 divide-y-2 divide-black">
+              {" "}
+              {pill?.times.map((item) => {
+                return (
+                  <div key={item.time} className="text-2xl">
+                    {item.time} น.
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-x-2">
+          <h1 className="text-3xl text-center p-5">
+            คุณสมบัติของ <b>{pill?.medicine.name}</b>
+          </h1>
+          <div className="flex w-1/2 px-10 py-4">
+            <b className="font-bold text-2xl px-6 text-right">สรรพคุณ : </b>
+            <div className="text-2xl">{pill?.medicine.description}</div>
+          </div>
+          <div className="flex w-1/2 px-10 py-4">
+            <b className="font-bold text-2xl px-6 text-right">หมายเหตุ : </b>
+            <div className="text-2xl">{pill?.medicine.note}</div>
+          </div>
+          <div className="flex w-1/2 px-10 py-4">
+            <img src={pill?.medicine.img}></img>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
