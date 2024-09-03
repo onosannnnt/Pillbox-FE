e055526ea51aa0fb3bff7429a4c8893e2db52ca2
onosannnnt <onopingping@gmail.com>
2024-09-04 01:12:09 +0700

    ✨ feat : Pill stock page

--- src/pages/PillStock.tsx
+++ src/pages/PillStock.tsx
@@ -1,80 +1,105 @@
-import { Table, TableColumnsType } from 'antd'
-import React from 'react'
+import { axiosInstance } from "@/utils/axios";
+import { Table, TableColumnsType } from "antd";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 type TablePillStock = {
-  stockId: number
-  pillName: string
-  totalAmount: number
-  amount: number
-  unit: string
-}
+  channelIndex: number;
+  medicine: MedicineType;
+  total: number;
+  amount: number;
+  unit: string;
+};
+
+type MedicineType = {
+  description: string;
+  name: string;
+  id: string;
+  img: string;
+  note: string;
+};
 
 const pillStockColumns: TableColumnsType<TablePillStock> = [
   {
-    title: 'ช่องที่',
-    dataIndex: 'stockId',
-    key: 'stockId',
+    title: "ช่องที่",
+    dataIndex: "channelIndex",
+    key: "channelIndex",
+    render: (channelIndex) => <span>{channelIndex + 1}</span>,
   },
   {
-    title: 'ชื่อยาสามัญ',
-    dataIndex: 'pillName',
-    key: 'pillName',
+    title: "ชื่อยาสามัญ",
+    dataIndex: "medicine",
+    key: "medicine",
+    render: (medicine) => <span>{medicine.name}</span>,
   },
   {
-    title: 'จำนวนยาทั้งหมด',
-    dataIndex: 'totalAmount',
-    key: 'totalAmount',
-    render: (_, record) => (
-      <span>
-        {record.totalAmount} {record.unit}
-      </span>
-    ),
+    title: "จำนวนยาทั้งหมด",
+    dataIndex: "total",
+    key: "total",
+    render: (_, record) => <span>{record.total} เม็ด</span>,
   },
   {
-    title: 'จำนวนยาคงเหลือ',
-    dataIndex: 'amount',
-    key: 'amount',
-    render: (_, record) => (
-      <span>
-        {record.amount} {record.unit}
-      </span>
-    ),
+    title: "จำนวนยาคงเหลือ",
+    dataIndex: "amount",
+    key: "amount",
+    render: (_, record) => <span>{record.amount} เม็ด</span>,
   },
-]
+];
 
 const PillStock: React.FC = () => {
-  const mockData: TablePillStock[] = [
-    {
-      stockId: 1,
-      pillName: 'ยาลดความดัน',
-      totalAmount: 100,
-      amount: 10,
-      unit: 'เม็ด',
-    },
-    {
-      stockId: 2,
-      pillName: 'ยาลดความดัน',
-      totalAmount: 100,
-      amount: 10,
-      unit: 'เม็ด',
-    },
-    {
-      stockId: 3,
-      pillName: 'ยาลดความดัน',
-      totalAmount: 100,
-      amount: 10,
-      unit: 'เม็ด',
-    },
-  ]
+  const [data, setData] = useState<TablePillStock[]>([]);
+  const fetchPill = async () => {
+    try {
+      const { data } = await axiosInstance.get("/user/getPillChannels");
+      setData(data);
+      console.log(data);
+    } catch {
+      Swal.fire({
+        icon: "error",
+        title: "เกิดข้อผิดพลาด",
+        text: "กรุณาลองใหม่อีกครั้ง",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchPill();
+  }, []);
 
   return (
     <React.Fragment>
       <div className="container p-8 flex flex-col gap-y-4">
-        <h1 className="text-2xl font-bold text-center">ประวัติการใช้งานกล่องยา</h1>
-        <Table columns={pillStockColumns} dataSource={mockData} />
+        <h1 className="text-2xl font-bold text-center">
+          ประวัติการใช้งานกล่องยา
+        </h1>
+        <Table columns={pillStockColumns} dataSource={data} />
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default PillStock
+export default PillStock;
+
+// const mockData: TablePillStock[] = [
+//   {
+//     stockId: 1,
+//     pillName: "ยาลดความดัน",
+//     totalAmount: 100,
+//     amount: 10,
+//     unit: "เม็ด",
+//   },
+//   {
+//     stockId: 2,
+//     pillName: "ยาลดความดัน",
+//     totalAmount: 100,
+//     amount: 10,
+//     unit: "เม็ด",
+//   },
+//   {
+//     stockId: 3,
+//     pillName: "ยาลดความดัน",
+//     totalAmount: 100,
+//     amount: 10,
+//     unit: "เม็ด",
+//   },
+// ];
