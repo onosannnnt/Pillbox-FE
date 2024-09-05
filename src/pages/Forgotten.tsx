63e44b7008fb95c929904305c9ea22a73f0f6d93
onosannnnt <onopingping@gmail.com>
2024-09-06 00:46:00 +0700

    ✨ feat : forgotten page

--- src/pages/Forgotten.tsx
+++ src/pages/Forgotten.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import {
   LineChart,
   Line,
@@ -9,44 +9,124 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import { Select } from "antd";
+import Swal from "sweetalert2";
+import { axiosInstance } from "@/utils/axios";
+import Loading from "@/components/Loading";
 
-const Forgotten = () => {
-  const data = [
-    { name: "วันอาทิตย์", forgotten: 5 },
-    { name: "วันจันทร์", forgotten: 2 },
-    { name: "วันอังคาร", forgotten: 8 },
-    { name: "วันพุธ", forgotten: 4 },
-    { name: "วันพฤหัส", forgotten: 4 },
-    { name: "วันศุกร์", forgotten: 2 },
-    { name: "วันเสาร์", forgotten: 3 },
-  ];
+type forgottenType = {
+  id: string;
+  task: string;
+  createdAt: Date;
+  medicine: medicineType;
+};
+
+type medicineType = {
+  id: string;
+  name: string;
+  description: string;
+  note: string;
+  img: string;
+};
+
+const Forgotten: FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [forgotten, setForgotten] = useState<forgottenType[]>([]);
+  const [medicine, setMedicine] = useState<medicineType[]>([]);
+  const [filter, setFilter] = useState<string>("all");
+  const fetchMedicine = async () => {
+    try {
+      const response = await axiosInstance("/user/getMedicines");
+      setMedicine([
+        {
+          id: "",
+          name: "all",
+          description: "",
+          note: "",
+          img: "",
+        },
+        ...response.data,
+      ]);
+    } catch {
+      Swal.fire({
+        icon: "error",
+        title: "เกิดข้อผิดพลาด",
+        text: "ไม่สามารถดึงข้อมูลยาได้",
+      });
+    }
+  };
+
+  const fetchForgotten = async () => {
+    const data = [
+      { name: "วันอาทิตย์", forgotten: 0 },
+      { name: "วันจันทร์", forgotten: 0 },
+      { name: "วันอังคาร", forgotten: 0 },
+      { name: "วันพุธ", forgotten: 0 },
+      { name: "วันพฤหัส", forgotten: 0 },
+      { name: "วันศุกร์", forgotten: 0 },
+      { name: "วันเสาร์", forgotten: 0 },
+    ];
+    try {
+      const response = await axiosInstance("/user/getForgetHistory");
+      response.data.forEach((item: forgottenType) => {
+        const date = new Date(item.createdAt).getDay();
+        if (filter !== "all" && item.medicine.name !== filter) return;
+        data[date].forgotten += 1;
+      });
+      setForgotten(data as unknown as forgottenType[]);
+    } catch {
+      Swal.fire({
+        icon: "error",
+        title: "เกิดข้อผิดพลาด",
+        text: "ไม่สามารถดึงข้อมูลการลืมยาได้",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchMedicine();
+    fetchForgotten();
+    setIsLoading(false);
+  }, [filter]);
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="w-full h-screen content-center text-center">
+          <Loading size="large" />
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-      <div className="container p-8 flex flex-col gap-y-4">
+      <div className="container p-8 flex flex-col gap-y-4 h-full">
         <h1 className="text-2xl font-bold text-center">อัตราการลืมทานยา</h1>
         <h2 className="text-1xl font-bold text-center">
-          ตั้งแต่วันที่ --/--/-- ถึง --/--/--
+          ตั้งแต่วันที่{" "}
+          {new Date(
+            new Date().setDate(new Date().getDate() - 6)
+          ).toLocaleDateString("th-TH", {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+          })}{" "}
+          / ถึง {new Date().toLocaleDateString()}
         </h2>
         <div className="flex w-full justify-end">
           <Select
             defaultValue="all"
             style={{ width: "20%" }}
-            options={[
-              { value: "all", label: "ทั้งหมด" },
-              { value: "1", label: "1" },
-              { value: "2", label: "2" },
-              { value: "3", label: "3" },
-              { value: "4", label: "4" },
-              { value: "5", label: "5" },
-              { value: "6", label: "6" },
-              { value: "7", label: "7" },
-              { value: "8", label: "8" },
-            ]}
+            options={medicine.map((item) => ({
+              label: item.name === "all" ? "ทั้งหมด" : item.name,
+              value: item.name,
+            }))}
+            onChange={(value) => setFilter(value)}
           />
         </div>
-        <div className="flex justify-center ">
-          <ResponsiveContainer width="100%" height={600}>
-            <LineChart data={data}>
+        <div className="flex justify-center h-3/4">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={forgotten}>
               <Line type="monotone" dataKey="forgotten" stroke="#8884d8" />
               <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
               <XAxis dataKey="name" />
