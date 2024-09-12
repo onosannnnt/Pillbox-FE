cede2d8c539268bda072e620ea6034da8ad44374
onosannnnt <onopingping@gmail.com>
2024-09-12 16:30:37 +0700

    ✨ feat : admin page and admin sidebar

---
+++ src/components/AdminSideBar.tsx
@@ -0,0 +1,67 @@
+import { BASE_ROUTE, ADMIN_ROUTE } from "@/config/route";
+import { AuthContext } from "@/context/auth";
+import { axiosInstance } from "@/utils/axios";
+import { Button } from "antd";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const AdminSideBar: React.FC = () => {
+  const auth = useContext(AuthContext);
+  const handleLogout = async () => {
+    try {
+      const response = await axiosInstance.get("/user/logout");
+      if (response.status != 200) {
+        throw new Error("Logout failed");
+      }
+      Swal.fire({
+        icon: "success",
+        title: "ออกจากระบบสำเร็จ",
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        window.location.href = BASE_ROUTE;
+      });
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "ระบบขัดข้อง",
+        text: "กรุณาลองใหม่อีกครั้ง",
+      });
+      console.log(error);
+      return;
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div className="bg-primary-blue text-black flex h-full w-80 flex-col justify-between py-[4rem]">
+        <div className="flex flex-col justify-center">
+          <img
+            src="klongyaa.png"
+            alt="icon"
+            className="w-64 aspect-square self-center"
+          />
+          <div className="flex flex-col gap-y-4 text-xl">
+            <p className="text-center text-2xl ">{auth?.auth.username}</p>
+            <Link to={ADMIN_ROUTE}>
+              <p className="text-center hover:bg-secondary-blue p-2">
+                Dashboard
+              </p>
+            </Link>
+            <Link to={BASE_ROUTE}>
+              <p className="text-center hover:bg-secondary-blue p-2">Home</p>
+            </Link>
+          </div>
+        </div>
+        <div className="flex w-full justify-center">
+          <Button className="bg-secondary-blue" onClick={handleLogout}>
+            ออกจากระบบ
+          </Button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default AdminSideBar;
--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -1,36 +1,38 @@
-import SideBar from '@/components/SideBar'
-import { LOGIN_ROUTE } from '@/config/route'
-import { AuthContext, TRole } from '@/context/auth'
-import React, { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { LOGIN_ROUTE } from "@/config/route";
+import { AuthContext, TRole } from "@/context/auth";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AdminSideBar from "../AdminSideBar";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const AdminLayout: React.FC<Props> = (props) => {
-  const navigate = useNavigate()
-  const authContext = useContext(AuthContext)
+  const navigate = useNavigate();
+  const authContext = useContext(AuthContext);
   useEffect(() => {
-    const AllowRole: TRole[] = ['admin']
+    const AllowRole: TRole[] = ["admin"];
     if (authContext?.auth.isAuth === false) {
-      navigate(LOGIN_ROUTE)
+      navigate(LOGIN_ROUTE);
     }
     if (authContext?.auth.role && !AllowRole.includes(authContext.auth.role)) {
-      navigate(LOGIN_ROUTE)
+      navigate(LOGIN_ROUTE);
     }
-  }, [authContext, navigate])
+  }, [authContext, navigate]);
 
   return (
     <React.Fragment>
       <div className="flex w-full">
         <div className="sticky left-0 top-0 h-screen bg-white">
-          <SideBar />
+          <AdminSideBar />
         </div>
-        <div className="w-full m-[2rem] rounded-2xl bg-secondary-blue bg-opacity-25">{props.children}</div>
+        <div className="w-full m-[2rem] rounded-2xl bg-secondary-blue bg-opacity-25">
+          {props.children}
+        </div>
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
