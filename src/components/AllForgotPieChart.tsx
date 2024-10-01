94e2188e550977747cccfdfc621bf11c199c4008
onosannnnt <onopingping@gmail.com>
2024-10-01 18:16:19 +0700

    🐛 feat :  all forget Pie chart

---
+++ src/components/AllForgotPieChart.tsx
@@ -0,0 +1,97 @@
+import { axiosInstance } from "@/utils/axios";
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
+import Swal from "sweetalert2";
+
+// Sample data
+type dataType = {
+  firstName: string;
+  user_username: string;
+  user: string;
+  forget: number;
+};
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const getPercentage = (value: number, total: number) =>
+  ((value / total) * 100).toFixed(2);
+
+const AllForgotPieChart = () => {
+  const [forgetData, setForgetData] = useState<dataType[]>([]);
+  const [allTotal, setAllTotal] = useState<number>(0);
+
+  const fetchAllForgot = async () => {
+    try {
+      const response = await axiosInstance.get("/admin/getAllForgetHistory");
+      console.log(response.data);
+      setAllTotal(
+        response.data.reduce(
+          (sum: number, entry: dataType) =>
+            sum + parseInt(entry.forget.toString()),
+          0
+        )
+      );
+      setForgetData(response.data);
+      console.log(allTotal);
+    } catch {
+      Swal.fire({
+        title: "Error!",
+        text: "Something went wrong",
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchAllForgot();
+  }, []);
+  return (
+    <>
+      <div className="w-full bg-white rounded-lg px-5">
+        <div className="text-center text-2xl">จำนวนการลืมทานยาทั้งหมด</div>
+        <div className="w-full grid grid-cols-2">
+          <ResponsiveContainer width="100%" height={300}>
+            {/* Responsive container */}
+            <PieChart>
+              <Pie
+                data={forgetData}
+                cx="50%" // Center the pie chart horizontally
+                cy="50%" // Center the pie chart vertically
+                innerRadius="60%" // Responsive inner radius
+                outerRadius="80%" // Responsive outer radius
+                fill="#8884d8"
+                paddingAngle={5}
+                dataKey="forget"
+                // label={({ value }) => `${getPercentage(value, allTotal)}%`} // Display percentage in the label
+              >
+                {forgetData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip
+                formatter={(value, name, props) => [
+                  `${getPercentage(Number(value), allTotal)}%`,
+                  `${props.payload.firstName || props.payload.user_username}`,
+                ]}
+              />{" "}
+            </PieChart>
+          </ResponsiveContainer>
+          <div className="flex flex-col justify-around divide-y-2 divide-gray-950 overflow-auto">
+            {forgetData.map((item, index) => (
+              <div key={index} className="flex justify-between ">
+                <div>{item.firstName || item.user_username || item.user}</div>
+                <div>{getPercentage(item.forget, allTotal)}%</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AllForgotPieChart;
--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import AllForgot from "@/components/AllForgot";
+import AllForgotPieChart from "@/components/AllForgotPieChart";
 
 const Dashboard = () => {
   return (
     <>
       <div className="h-full w-full flex flex-col items-center gap-10 py-10">
         <h1 className="text-4xl">ภาพรวม</h1>
-        <div className="w-full flex justify-around">
-          <div className="w-1/4 border">1</div>
-          <div className="w-1/4 border">1</div>
-          <div className="w-1/4 border">1</div>
+        <div className="w-full h-fit flex justify-around">
+          <div className="h-full w-2/5 flex gap-10">
+            <AllForgotPieChart />
+          </div>
+          <div className="h-full w-2/5 flex gap-10"></div>
         </div>
         <AllForgot />
       </div>
